e957585b721ac537b9c0fdd9e35e149b70ed301f
ethancheez <61472206+ethancheez@users.noreply.github.com>
2024-07-04 20:41:33 -1000

    Update skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -64,6 +64,26 @@ const Skills = () => {
             </svg>
             <h4>C/C++</h4>
           </Col>
+          <Col className="python-icon icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width={iconDimensions.width}
+              height={iconDimensions.height}
+              viewBox="0 0 48 48"
+            >
+              <path
+                fill="#0277BD"
+                d="M24.047,5c-1.555,0.005-2.633,0.142-3.936,0.367c-3.848,0.67-4.549,2.077-4.549,4.67V14h9v2H15.22h-4.35c-2.636,0-4.943,1.242-5.674,4.219c-0.826,3.417-0.863,5.557,0,9.125C5.851,32.005,7.294,34,9.931,34h3.632v-5.104c0-2.966,2.686-5.896,5.764-5.896h7.236c2.523,0,5-1.862,5-4.377v-8.586c0-2.439-1.759-4.263-4.218-4.672C27.406,5.359,25.589,4.994,24.047,5z M19.063,9c0.821,0,1.5,0.677,1.5,1.502c0,0.833-0.679,1.498-1.5,1.498c-0.837,0-1.5-0.664-1.5-1.498C17.563,9.68,18.226,9,19.063,9z"
+              ></path>
+              <path
+                fill="#FFC107"
+                d="M23.078,43c1.555-0.005,2.633-0.142,3.936-0.367c3.848-0.67,4.549-2.077,4.549-4.67V34h-9v-2h9.343h4.35c2.636,0,4.943-1.242,5.674-4.219c0.826-3.417,0.863-5.557,0-9.125C41.274,15.995,39.831,14,37.194,14h-3.632v5.104c0,2.966-2.686,5.896-5.764,5.896h-7.236c-2.523,0-5,1.862-5,4.377v8.586c0,2.439,1.759,4.263,4.218,4.672C19.719,42.641,21.536,43.006,23.078,43z M28.063,39c-0.821,0-1.5-0.677-1.5-1.502c0-0.833,0.679-1.498,1.5-1.498c0.837,0,1.5,0.664,1.5,1.498C29.563,38.32,28.899,39,28.063,39z"
+              ></path>
+            </svg>
+            <h4>Python</h4>
+          </Col>
           <Col className="html5-icon icon">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -115,26 +135,6 @@ const Skills = () => {
               </g>
             </svg>
             <h4>PHP</h4>
-          </Col>
-          <Col className="python-icon icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              width={iconDimensions.width}
-              height={iconDimensions.height}
-              viewBox="0 0 48 48"
-            >
-              <path
-                fill="#0277BD"
-                d="M24.047,5c-1.555,0.005-2.633,0.142-3.936,0.367c-3.848,0.67-4.549,2.077-4.549,4.67V14h9v2H15.22h-4.35c-2.636,0-4.943,1.242-5.674,4.219c-0.826,3.417-0.863,5.557,0,9.125C5.851,32.005,7.294,34,9.931,34h3.632v-5.104c0-2.966,2.686-5.896,5.764-5.896h7.236c2.523,0,5-1.862,5-4.377v-8.586c0-2.439-1.759-4.263-4.218-4.672C27.406,5.359,25.589,4.994,24.047,5z M19.063,9c0.821,0,1.5,0.677,1.5,1.502c0,0.833-0.679,1.498-1.5,1.498c-0.837,0-1.5-0.664-1.5-1.498C17.563,9.68,18.226,9,19.063,9z"
-              ></path>
-              <path
-                fill="#FFC107"
-                d="M23.078,43c1.555-0.005,2.633-0.142,3.936-0.367c3.848-0.67,4.549-2.077,4.549-4.67V34h-9v-2h9.343h4.35c2.636,0,4.943-1.242,5.674-4.219c0.826-3.417,0.863-5.557,0-9.125C41.274,15.995,39.831,14,37.194,14h-3.632v5.104c0,2.966-2.686,5.896-5.764,5.896h-7.236c-2.523,0-5,1.862-5,4.377v8.586c0,2.439,1.759,4.263,4.218,4.672C19.719,42.641,21.536,43.006,23.078,43z M28.063,39c-0.821,0-1.5-0.677-1.5-1.502c0-0.833,0.679-1.498,1.5-1.498c0.837,0,1.5,0.664,1.5,1.498C29.563,38.32,28.899,39,28.063,39z"
-              ></path>
-            </svg>
-            <h4>Python</h4>
           </Col>
           <Col className="java-icon icon">
             <svg
@@ -318,6 +318,14 @@ const Skills = () => {
             />
             <h4>MATLAB</h4>
           </Col>
+          <Col className="rust-icon icon">
+            <Image
+              src="https://www.rust-lang.org/logos/rust-logo-512x512.png"
+              width={iconDimensions.width}
+              height={iconDimensions.height}
+            />
+            <h4>Rust</h4>
+          </Col>
         </Row>
 
         <Row style={{ marginTop: "3em" }}>
@@ -376,6 +384,14 @@ const Skills = () => {
           <Col className="solder-icon icon">
             <Image src="https://cdn-icons-png.flaticon.com/512/2333/2333619.png" height={iconDimensions.height} />
             <h4>Soldering</h4>
+          </Col>
+          <Col className="3d-print-icon icon">
+            <Image src="https://cdn-icons-png.flaticon.com/512/4841/4841317.png" height={iconDimensions.height - 10} style={{ marginBottom: 10 }} />
+            <h4>3D Printing</h4>
+          </Col>
+          <Col className="fusion-icon icon">
+            <Image src="https://damassets.autodesk.net/content/dam/autodesk/www/product-imagery/badge-150x150/fusion-badge-150x150.png" height={iconDimensions.height - 10} style={{ marginBottom: 10 }} />
+            <h4>Autodesk Fusion</h4>
           </Col>
           <Col className="eagle-icon icon">
             <Image src="https://yt3.googleusercontent.com/ytc/AIdro_kjV0lXOd-pv-75AD_6kWpkASe7EkBWsOTPqAn0Ow=s900-c-k-c0x00ffffff-no-rj" height={iconDimensions.height - 10} style={{ marginBottom: 10 }} />
